fb1f1dad8a12938c2f5d9647dee51989992decb8
ChenQiyue-cloud <55774342+ChenQiyue-cloud@users.noreply.github.com>
2020-04-01 15:26:19 +0800

    Create 6.2.c

---
+++ 6.2.c
@@ -0,0 +1,23 @@
+/*
+使用嵌套循环，按下面的格式打印字符：
+$
+$$
+$$$
+$$$$
+$$$$$
+*/
+#include<stdio.h>
+int main()
+{
+    int i,j;
+
+    for ( i=0; i<5; i++ )
+    {
+        for ( j=0; j<=i; j++ )
+        {
+            printf("$");
+        }
+        printf("\n");
+    }
+    return 0;
+}
